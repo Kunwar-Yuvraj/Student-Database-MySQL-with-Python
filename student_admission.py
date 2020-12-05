7cf36e1f5fec160f3ac229882bc1352cfb7b38e7
Kunwar-Yuvraj <cppprograminglearner@gmail.com>
2020-12-05 21:08:32 +0530

    Improved

--- student_admission.py
+++ student_admission.py
@@ -1,35 +1,35 @@
 # Student admission database
 import mysql.connector
 
+
 class Database_Helper:
     # Constructor
-    def __init__(self):
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+
         self.con = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            port="3306",
-            password="jayant",
-            database="Students"
+            host=host, user=user, password=password, database=database
         )
 
         str_query = "create table if not exists Student(Student_ID int primary key, Student_Name varchar(100), Phone_Number varchar(12), Fee_Status varchar(2))"
         mycursor = self.con.cursor()
         mycursor.execute(str_query)
-     
-    
+
     # Insertion method
     def insert_Data(self, id, name, phone, fee):
-        str_query="insert into Student(Student_ID, Student_Name, Phone_Number, Fee_Status) values({},'{}','{}','{}')".format(id,name,phone, fee)
-        cur=self.con.cursor()
+        str_query = f"insert into Student(Student_ID, Student_Name, Phone_Number, Fee_Status) values({id},'{name}','{phone}','{fee}')"
+        cur = self.con.cursor()
         cur.execute(str_query)
         self.con.commit()
-        print("Student {} saved to Database".format(name))
-
+        print(f"Student {name} saved to Databasex\n")
 
     # Fetching all data
     def fetch_data(self):
-        str_query="select * from student"
-        cur=self.con.cursor()
+        str_query = "select * from Student"
+        cur = self.con.cursor()
         cur.execute(str_query)
         for record in cur:
             print(f"Student_ID: {record[0]}")
@@ -39,81 +39,90 @@ class Database_Helper:
             print()
             print()
 
-
     # Deletion Method
-    def delete(self,id):
-        str_query="delete from student where Student_ID = {}".format(id)
-        cur=self.con.cursor()
+    def delete(self, id):
+        str_query = "delete from Student where Student_ID = {}".format(id)
+        cur = self.con.cursor()
         cur.execute(str_query)
         self.con.commit()
         print("Student with id {} is deleted\n".format(id))
-     
-
 
     # Updation method
-    def update_User(self,id,newname, newphone):
-        str_query="update student set Student_Name='{}', Phone_Number='{}' where Student_ID={}".format(newname, newphone,id)
-        cur=self.con.cursor()
+    def update_User(self, id, newname, newphone, newstatus):
+        str_query = "update Student set Student_Name='{}', Phone_Number='{}' where Student_ID={}".format(
+            newname, newphone, id
+        )
+        cur = self.con.cursor()
         cur.execute(str_query)
-        self.con.commit()
-        print("Student with Id {} updated\n".format(id))
-     
- 
+        print(f"\nStudent with Id {id} updated\n")
 
 
 # Main function
 if __name__ == "__main__":
-    db=Database_Helper()
-    print("***** WELCOME TO SCHOOL MANAGEMENT SYSTEM *****")
-    while(True):
+    host = input("Enter host: ")
+    user = input("Enter user name: ")
+    password = input("Enter password: ")
+    database = input("Enter database: ")
+    db = Database_Helper(host, user, password, database)
+    print("\n***** WELCOME TO SCHOOL MANAGEMENT SYSTEM *****\n")
+    while True:
         print("Press 1 to insert new Student")
         print("Press 2 to display all Students")
         print("Press 3 to delete a Student")
         print("Press 4 to update a Student")
-        print("Press 5 to exit the program")
+        print("Press 5 to exit the program\n")
         try:
-            choice=int(input())
+            choice = int(input("Enter your choice: "))
+            print()
 
-            if(choice==1):
-                #Insert Student
-                uid=int(input("Enter Student ID: "))
-                uname=input("Enter Student Name: ")
-                uphone=input("Enter Student's Phone: ")
-                ustatus=input("Enter Fee Status.'y' for paid and 'n' for not paid: ")[0]
-                db.insert_Data(uid,uname, uphone,ustatus)
+            if choice == 1:
+                # Insert Student
+                uid = int(input("Enter Student ID: "))
+                uname = input("Enter Student Name: ")
+                uphone = input("Enter Student's Phone: ")
+                ustatus = input("Enter Fee Status.'y' for paid and 'n' for not paid: ")[
+                    0
+                ]
+                db.insert_Data(uid, uname, uphone, ustatus)
                 pass
 
-
-            elif choice==2:
-                #display Student
+            elif choice == 2:
+                # display Student
                 db.fetch_data()
                 pass
 
-
-            elif choice==3:
-                #delete Student
-                uid=int(input("Enter ID to be deleted: "))
+            elif choice == 3:
+                # delete Student
+                uid = int(input("Enter ID to be deleted: "))
                 db.delete(uid)
                 pass
 
+            elif choice == 4:
+                # Update Student
+                uid = int(input("Enter Student ID: "))
+                uname = input("Enter new name: ")
+                uphone = input("Enter new phone number: ")
+                ustatus = input(
+                    "Enter new Fee Status. 'yes' for paid and 'no' for not paid: "
+                )
 
-            elif choice==4:
-                #Update Student
-                uid=int(input("Enter Student ID: "))
-                uname=input("Enter new name: ")
-                uphone=input("Enter new phone number: ")
-                ustatus=input("Enter new Fee Status. 'yes' for paid and 'no' for not paid: ")
-                db.update_User(uid,uname,uphone,ustatus)
+                if (
+                    ustatus == "Yes"
+                    or ustatus == "yes"
+                    or ustatus == "No"
+                    or ustatus == "no"
+                ):
+                    db.update_User(uid, uname, uphone, ustatus)
 
+                else:
+                    print("Invalid choice")
 
-            elif choice==5:
+            elif choice == 5:
                 break
-
 
             else:
                 print("Invalid input! Please try again")
-                
-                
+
         except Exception as e:
             print(e)
             print("Invalid input! Try again")
